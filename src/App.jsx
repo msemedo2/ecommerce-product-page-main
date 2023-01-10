263ce877bd48620cc20d0baf3feca089fdd1bcf4
unknown <miguel.semedo.2@outlook.com>
2023-01-10 15:21:56 +0000

    reset styles. add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.scss';
 
 function App() {
-	return <div className="app-container"></div>;
+	return (
+		<div className="app-container">
+			<div>
+				Phone A Friend 9.6 XXLAYNA MARIE & ANTON HARDEN
+				<img src="./assets/image-product-1.jpg" alt="" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
