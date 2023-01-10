3e319ef1b66ee339444d11fa2856d716cc33d19c
unknown <miguel.semedo.2@outlook.com>
2023-01-10 22:42:11 +0000

    product image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
+
+import ProductImage from './components/product-image/product-image.component';
 import './App.scss';
 
-function App() {
+const App = () => {
+	const [image, setImage] = useState('1');
+	const thumbnails = [
+		{
+			id: 1,
+			thumbnail: 'image-product-1-thumbnail.jpg',
+		},
+		{
+			id: 2,
+			thumbnail: 'image-product-2-thumbnail.jpg',
+		},
+		{
+			id: 3,
+			thumbnail: 'image-product-3-thumbnail.jpg',
+		},
+		{
+			id: 4,
+			thumbnail: 'image-product-4-thumbnail.jpg',
+		},
+	];
+
+	const handleHover = (e) => {
+		const { id } = e.target;
+		setImage(id);
+	};
+
 	return (
 		<div className="app-container">
-			<div>
-				Phone A Friend 9.6 XXLAYNA MARIE & ANTON HARDEN
-				<img src="./assets/image-product-1.jpg" alt="" />
-			</div>
+			<ProductImage
+				thumbnails={thumbnails}
+				handleHover={handleHover}
+				image={image}
+			/>
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/components/product-image/product-image.component.jsx
@@ -0,0 +1,32 @@
+import './product-image.scss';
+
+const ProductImage = ({ thumbnails, handleHover, image }) => {
+	return (
+		<>
+			<div className="image-product-container">
+				<img
+					className="image-product"
+					src={`../public/image-product-${image}.jpg`}
+					alt="image product"
+				/>
+			</div>
+			<div className="thumbnail-container">
+				{thumbnails.map(({ thumbnail, id }) => {
+					return (
+						<div className="thumbnail-image-container" key={id}>
+							<img
+								onMouseOver={handleHover}
+								className="thumbnail-image"
+								id={id}
+								src={`../public/${thumbnail}`}
+								alt="thumbnail-image"
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
+
+export default ProductImage;
